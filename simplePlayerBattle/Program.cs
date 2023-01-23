1ba313252285b5e10971c1e1d26aedebf53a66e6
desired <desired@DESKTOP-BNNTGOI>
2023-01-23 10:01:37 +0200

    Добавьте файлы проекта.

---
+++ simplePlayerBattle/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int playerHealth=100;
+        int playerDamage=25;
+        int enemyHealth=300;
+        int enemyDamage=10;
+
+        while(playerHealth>0&&enemyHealth>0)
+        {
+            playerHealth -= enemyDamage;
+            enemyHealth -= playerDamage;
+
+            Console.WriteLine(playerHealth + "Игрок");
+            Console.WriteLine(enemyHealth + "Враг");
+        }
+
+        if(playerHealth<=0 && enemyHealth <= 0)
+        {
+            Console.WriteLine("Ничья");
+        }
+        else if (enemyHealth <= 0)
+        {
+            Console.WriteLine("Победа игрока");
+        }
+        else if (playerHealth <= 0)
+        {
+            Console.WriteLine("Победа врага");
+        }
+    }
+}
